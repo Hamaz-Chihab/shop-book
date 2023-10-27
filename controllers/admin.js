6dbe6f0040e78be2889e5eb96a33b5712d4afc1e
Hamaz-Chihab <c_hamaz@estin.dz>
2023-10-27 22:20:01 +0100

    fixing the error of the query parames

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,26 +1,23 @@
-const Products = require("../modules/product"); //importing the class from module file
+var bodyParser = require("body-parser");
 
-//middleware function of admin route :
-exports.getEditproduct = (req, res, next) => {
-  const editMode = req.query.edit; //have "true" id the query param exist
-  if (!editMode) {
-    return res.redirect("/");
-  }
-  const prodId = req.params.productID; //to have the access to the productID
-  Product.findById(prodId, (product) => {
-    if (!prodId) {
-      return res.redirect("/");
-    }
-    res.render("admin/edit-product", {
-      titlePage: "edit-product",
-      path: "/edit-product",
-      editing: editMode, //verifier si le produit exist ??
-    });
+// for routes looking like this `/products?page=1&pageSize=50`
+// app.get('/products', function(req, res) {
+//   const page = req.query.page;
+//   const pageSize = req.query.pageSize;
+//   res.send(`Filter with parameters ${page} and ${pageSize});`
+// });
+
+const Product = require("../modules/product"); //importing the class from module file
+exports.getAddProduct = (req, res, next) => {
+  res.render("admin/edit-product.ejs", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false,
   });
 };
 
 //middleware function of admin route :
-exports.postAddproduct = (req, res, next) => {
+exports.postAddProduct = (req, res, next) => {
   const title = req.body.title; //the name attribut in the ejs file
   const imageUrl = req.body.imageUrl; //the name attribut in the ejs file
   const price = req.body.price; //the name attribut in the ejs file
@@ -29,10 +26,32 @@ exports.postAddproduct = (req, res, next) => {
   product.save(); //save(push) the objetc in the array
   res.redirect("/");
 };
+//middleware function of admin route :
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.editing; //have "true" id the query param exist
+  if (!editMode) {
+    console.log("this is an error in params ??");
+    console.log(editMode);
+    return res.redirect("/");
+  }
+  const prodId = req.params.productID; //to have the access to the productID
+  Product.findById(prodId, (product) => {
+    if (!prodId) {
+      console.log("this is an error in the rendering ??");
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      titlePage: "Edit-product",
+      path: "admins/edit-product",
+      editing: editMode, //verifier si le produit exsist ??
+    });
+  });
+};
 
 //products middleware in admin route
 exports.getProducts = (req, res, next) => {
-  Products.fetchAll((product) => {
+  Product.fetchAll((product) => {
     res.render("admin/products", {
       //the ejs file to render it
       prods: product, //prods is used for shop-route and products is in the server
--- routes/admin.js
+++ routes/admin.js
@@ -7,13 +7,16 @@ const path = require("path");
 
 //controle system:
 const adminController = require("../controllers/admin");
-//adim/add-product getmiddleware //add-product postmiddleware ::
-router.get("/edit-product", adminController.getEditproduct);
-router.post("/add-product", adminController.postAddproduct);
 
-router.get("/edit-product/:productID", adminController.getEditproduct);
-
-//adim/products postmiddleware :
 router.get("/products", adminController.getProducts);
 
+router.get("/add-product", adminController.getAddProduct);
+
+router.post("/add-product", adminController.postAddProduct);
+
+router.get("/edit-product/:productID", adminController.getEditProduct);
+
+//adim/products postmiddleware :
+
 module.exports = router; //we don't need to export the routes after controllers
+// hello frome node js i am chihab hamaz 
