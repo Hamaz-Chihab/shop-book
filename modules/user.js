214b6bd9dda53a15f0dd5fddf2eabebf61fd223a
Hamaz-Chihab <c_hamaz@estin.dz>
2023-12-07 11:30:35 +0100

    prepare to starting warking with mongoose

--- modules/user.js
+++ modules/user.js
@@ -1,121 +1,121 @@
-/* eslint-disable no-unused-vars */
-const mongodb = require("mongodb");
-const getDb = require("../util/dataBase").getDb; //to get intract with the DB
-const ObjectId = mongodb.ObjectId;
-class User {
-  constructor(userName, email, cart, _id) {
-    this.userName = userName;
-    this.email = email;
-    this.cart = cart;
-    this._id = _id;
-  }
-  save() {
-    const db = getDb(); //get access to my DB by calling getDb
-    return db.collection("users").insertOne(this);
-  }
+// /* eslint-disable no-unused-vars */
+// const mongodb = require("mongodb");
+// const getDb = require("../util/dataBase").getDb; //to get intract with the DB
+// const ObjectId = mongodb.ObjectId;
+// class User {
+//   constructor(userName, email, cart, _id) {
+//     this.userName = userName;
+//     this.email = email;
+//     this.cart = cart;
+//     this._id = _id;
+//   }
+//   save() {
+//     const db = getDb(); //get access to my DB by calling getDb
+//     return db.collection("users").insertOne(this);
+//   }
 
-  addToCart(product) {
-    console.log("this is the cart In AddToCart : ", this);
-    console.log("HELLO WORLD from AddToCart : ", this.cart.items);
-    const cartProductIndex = this.cart.items.findIndex((cp) => {
-      return cp.productId.toString() == product._id.toString(); //=== mean strict equality (value + type)   == mean equality in value
-    });
-    let newQuantity = 1;
-    const updatedCartItems = [...this.cart.items];
+//   addToCart(product) {
+//     console.log("this is the cart In AddToCart : ", this);
+//     console.log("HELLO WORLD from AddToCart : ", this.cart.items);
+//     const cartProductIndex = this.cart.items.findIndex((cp) => {
+//       return cp.productId.toString() == product._id.toString(); //=== mean strict equality (value + type)   == mean equality in value
+//     });
+//     let newQuantity = 1;
+//     const updatedCartItems = [...this.cart.items];
 
-    if (cartProductIndex >= 0) {
-      //the product Exesist in Cart => the index >= 0
-      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
-      updatedCartItems[cartProductIndex].quantity = newQuantity;
-    } else {
-      //if product doesn't exist in cart => index < 0
-      updatedCartItems.push({
-        productId: new ObjectId(product._id),
-        quantity: newQuantity,
-      });
-    }
-    const updatedCart = {
-      items: updatedCartItems,
-    };
-    const db = getDb();
-    return db.collection("users").updateOne(
-      { _id: new ObjectId(this._id) }, //filter
-      { $set: { cart: updatedCart } }
-    );
-  }
-  getCart() {
-    const db = getDb();
-    this.cart.items = this.cart.items || [];
-    console.log("HELLO WORLD After AddToCart :", this.cart.items);
-    const productIds = this.cart.items.map((i) => {
-      return i.productId;
-    });
-    return db
-      .collection("products")
-      .find({
-        _id: { $in: productIds },
-      })
-      .toArray()
-      .then((products) => {
-        return products.map((p) => {
-          const item = this.cart.items.find((i) => {
-            return i.productId.toString() === p._id.toString();
-          });
-          return {
-            ...p,
-            quantity: item ? item.quantity : 0,
-          };
-        });
-      });
-  }
-  deleteItemFromCart(productId) {
-    // const updatedCartItems = [...this.cart.items];
-    // updatedCartItems.filter((item) => {
-    //   return item.productId.toString() !== productId.toString();
-    // });
-    const updatedCartItems = this.cart.items.filter((item) => {
-      return item.productId.toString() !== productId.toString();
-    }); //this return the array  without the filtring acording to the condition
-    const db = getDb();
-    //storing In DB:
-    return db
-      .collection("users")
-      .updateOne(
-        { _id: new ObjectId(this._id) },
-        { $set: { cart: { items: updatedCartItems } } }
-      );
-  }
-  addOrder() {
-    const db = getDb();
-    return db
-      .collection("orders")
-      .insertOne(this.cart)
-      .then((result) => {
-        this.cart = { items: [] };
-        return db
-          .collection("users")
-          .updateOne(
-            { _id: new ObjectId(this._id) },
-            { $set: { cart: { items: [] } } }
-          );
-      });
-  }
-  getOrders() {
-    const db = getDb();
-    return db
-      .collection("orders")
-      .find({ "user._id": new ObjectId(this._id) })
-      .toArray();
-  }
-  static findById(userId) {
-    const db = getDb();
-    return db
-      .collection("users")
-      .findOne({ _id: new ObjectId(userId) })
-      .then((user) => {
-        return user;
-      })
-      .catch((err) => console.log("error in userfindById :", err));
-  }
-}
-module.exports = User;
+//     if (cartProductIndex >= 0) {
+//       //the product Exesist in Cart => the index >= 0
+//       newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+//       updatedCartItems[cartProductIndex].quantity = newQuantity;
+//     } else {
+//       //if product doesn't exist in cart => index < 0
+//       updatedCartItems.push({
+//         productId: new ObjectId(product._id),
+//         quantity: newQuantity,
+//       });
+//     }
+//     const updatedCart = {
+//       items: updatedCartItems,
+//     };
+//     const db = getDb();
+//     return db.collection("users").updateOne(
+//       { _id: new ObjectId(this._id) }, //filter
+//       { $set: { cart: updatedCart } }
+//     );
+//   }
+//   getCart() {
+//     const db = getDb();
+//     this.cart.items = this.cart.items || [];
+//     console.log("HELLO WORLD After AddToCart :", this.cart.items);
+//     const productIds = this.cart.items.map((i) => {
+//       return i.productId;
+//     });
+//     return db
+//       .collection("products")
+//       .find({
+//         _id: { $in: productIds },
+//       })
+//       .toArray()
+//       .then((products) => {
+//         return products.map((p) => {
+//           const item = this.cart.items.find((i) => {
+//             return i.productId.toString() === p._id.toString();
+//           });
+//           return {
+//             ...p,
+//             quantity: item ? item.quantity : 0,
+//           };
+//         });
+//       });
+//   }
+//   deleteItemFromCart(productId) {
+//     // const updatedCartItems = [...this.cart.items];
+//     // updatedCartItems.filter((item) => {
+//     //   return item.productId.toString() !== productId.toString();
+//     // });
+//     const updatedCartItems = this.cart.items.filter((item) => {
+//       return item.productId.toString() !== productId.toString();
+//     }); //this return the array  without the filtring acording to the condition
+//     const db = getDb();
+//     //storing In DB:
+//     return db
+//       .collection("users")
+//       .updateOne(
+//         { _id: new ObjectId(this._id) },
+//         { $set: { cart: { items: updatedCartItems } } }
+//       );
+//   }
+//   addOrder() {
+//     const db = getDb();
+//     return db
+//       .collection("orders")
+//       .insertOne(this.cart)
+//       .then((result) => {
+//         this.cart = { items: [] };
+//         return db
+//           .collection("users")
+//           .updateOne(
+//             { _id: new ObjectId(this._id) },
+//             { $set: { cart: { items: [] } } }
+//           );
+//       });
+//   }
+//   getOrders() {
+//     const db = getDb();
+//     return db
+//       .collection("orders")
+//       .find({ "user._id": new ObjectId(this._id) })
+//       .toArray();
+//   }
+//   static findById(userId) {
+//     const db = getDb();
+//     return db
+//       .collection("users")
+//       .findOne({ _id: new ObjectId(userId) })
+//       .then((user) => {
+//         return user;
+//       })
+//       .catch((err) => console.log("error in userfindById :", err));
+//   }
+// }
+// module.exports = User;
--- routes/shop.js
+++ routes/shop.js
@@ -3,25 +3,25 @@ const express = require("express");
 //path system :
 // const path = require("path");
 const router = express.Router();
+
+// //controller system:
+// const shopController = require("../controllers/shop");
+
+// router.get("/shop-cart", shopController.getCart);
+// router.post("/shop-cart", shopController.postCart);
+
+// // router.get("/shop-checkout", shopController.getCheckout);
+
+// router.get("/", shopController.getIndex); //the starting page
+
+// router.post("/shop-orders", shopController.postOrder);
+// router.get("/shop-orders", shopController.getOreder);
+
+// router.get("/shop-product/:productId", shopController.getProduct); //this is the Product-Detail.ejs use Dynamic route :the route will be defined by the ID od the product
+
+// router.get("/products", shopController.getProducts);
+
+// router.post("/cart-delete-item", shopController.postDeleteProduct);
+
+// router.get("/shop-products", shopController.getProducts);
 module.exports = router;
-
-//controller system:
-const shopController = require("../controllers/shop");
-
-router.get("/shop-cart", shopController.getCart);
-router.post("/shop-cart", shopController.postCart);
-
-// router.get("/shop-checkout", shopController.getCheckout);
-
-router.get("/", shopController.getIndex); //the starting page
-
-router.post("/shop-orders", shopController.postOrder);
-router.get("/shop-orders", shopController.getOreder);
-
-router.get("/shop-product/:productId", shopController.getProduct); //this is the Product-Detail.ejs use Dynamic route :the route will be defined by the ID od the product
-
-router.get("/products", shopController.getProducts);
-
-router.post("/cart-delete-item", shopController.postDeleteProduct);
-
-router.get("/shop-products", shopController.getProducts);
